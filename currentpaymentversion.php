b87157f817d61c168ecc15b31a0694bffe3659d3
pipeline <pipeline@jmaconsulting.biz>
2023-11-08 14:22:24 -0700

    Civix Upgrade to 23.11.8

--- currentpaymentversion.php
+++ currentpaymentversion.php
@@ -13,15 +13,6 @@ function currentpaymentversion_civicrm_config(&$config) {
 }
 
 /**
- * Implements hook_civicrm_xmlMenu().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_xmlMenu
- */
-function currentpaymentversion_civicrm_xmlMenu(&$files) {
-  _currentpaymentversion_civix_civicrm_xmlMenu($files);
-}
-
-/**
  * Implements hook_civicrm_install().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_install
@@ -31,39 +22,12 @@ function currentpaymentversion_civicrm_install() {
 }
 
 /**
- * Implements hook_civicrm_postInstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_postInstall
- */
-function currentpaymentversion_civicrm_postInstall() {
-  _currentpaymentversion_civix_civicrm_postInstall();
-}
-
-/**
- * Implements hook_civicrm_uninstall().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_uninstall
- */
-function currentpaymentversion_civicrm_uninstall() {
-  _currentpaymentversion_civix_civicrm_uninstall();
-}
-
-/**
  * Implements hook_civicrm_enable().
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_enable
  */
 function currentpaymentversion_civicrm_enable() {
   _currentpaymentversion_civix_civicrm_enable();
-}
-
-/**
- * Implements hook_civicrm_disable().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_disable
- */
-function currentpaymentversion_civicrm_disable() {
-  _currentpaymentversion_civix_civicrm_disable();
 }
 
 /**
@@ -109,7 +73,6 @@ function currentpaymentversion_civicrm_preProcess($formName, &$form) {
   }
 }
 
-
 function currentpaymentversion_civicrm_buildForm($formName, &$form) {
   if (Civi::settings()->get('contribution_invoice_settings')['show_latest_payments']) {
     if ((in_array($formName, ['CRM_Contribute_Form_AdditionalPayment', 'CRM_Contribute_Form_Contribution']) && ($id = $form->getVar('_id'))) ||
@@ -133,74 +96,6 @@ function currentpaymentversion_civicrm_buildForm($formName, &$form) {
   }
 }
 
-/**
- * Implements hook_civicrm_upgrade().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_upgrade
- */
-function currentpaymentversion_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
-  return _currentpaymentversion_civix_civicrm_upgrade($op, $queue);
-}
-
-/**
- * Implements hook_civicrm_managed().
- *
- * Generate a list of entities to create/deactivate/delete when this module
- * is installed, disabled, uninstalled.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_managed
- */
-function currentpaymentversion_civicrm_managed(&$entities) {
-  _currentpaymentversion_civix_civicrm_managed($entities);
-}
-
-/**
- * Implements hook_civicrm_caseTypes().
- *
- * Generate a list of case-types.
- *
- * Note: This hook only runs in CiviCRM 4.4+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_caseTypes
- */
-function currentpaymentversion_civicrm_caseTypes(&$caseTypes) {
-  _currentpaymentversion_civix_civicrm_caseTypes($caseTypes);
-}
-
-/**
- * Implements hook_civicrm_angularModules().
- *
- * Generate a list of Angular modules.
- *
- * Note: This hook only runs in CiviCRM 4.5+. It may
- * use features only available in v4.6+.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_angularModules
- */
-function currentpaymentversion_civicrm_angularModules(&$angularModules) {
-  _currentpaymentversion_civix_civicrm_angularModules($angularModules);
-}
-
-/**
- * Implements hook_civicrm_alterSettingsFolders().
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_alterSettingsFolders
- */
-function currentpaymentversion_civicrm_alterSettingsFolders(&$metaDataFolders = NULL) {
-  _currentpaymentversion_civix_civicrm_alterSettingsFolders($metaDataFolders);
-}
-
-/**
- * Implements hook_civicrm_entityTypes().
- *
- * Declare entity types provided by this module.
- *
- * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_entityTypes
- */
-function currentpaymentversion_civicrm_entityTypes(&$entityTypes) {
-  _currentpaymentversion_civix_civicrm_entityTypes($entityTypes);
-}
-
 // --- Functions below this ship commented out. Uncomment as required. ---
 
 /**
@@ -208,9 +103,8 @@ function currentpaymentversion_civicrm_entityTypes(&$entityTypes) {
  *
  * @link http://wiki.civicrm.org/confluence/display/CRMDOC/hook_civicrm_preProcess
  *
-function currentpaymentversion_civicrm_preProcess($formName, &$form) {
 
-} // */
+ // */
 
 /**
  * Implements hook_civicrm_navigationMenu().
